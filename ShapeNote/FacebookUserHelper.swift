4c26fc6ec1a4dfca2347d892a4bdc404b120f690
Charlie Williams <c@charliewilliams.org>
2015-11-06 23:32:05 +0000

    Facebook login and groups picker with filtering

---
+++ ShapeNote/FacebookUserHelper.swift
@@ -0,0 +1,9 @@
+//
+//  FacebookUserHelper.swift
+//  ShapeNote
+//
+//  Created by Charlie Williams on 06/11/2015.
+//  Copyright © 2015 Charlie Williams. All rights reserved.
+//
+
+import Foundation
