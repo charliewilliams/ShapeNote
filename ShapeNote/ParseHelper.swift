3bc56e313b2a0aa40ba2f67385cb4306b1ed3c6b
Charlie Williams <c@charliewilliams.org>
2015-11-21 17:20:09 +0000

    Parse

--- ShapeNote/ParseHelper.swift
+++ ShapeNote/ParseHelper.swift
@@ -7,3 +7,138 @@
 //
 
 import Foundation
+import Parse
+import CoreData
+
+typealias CompletionBlock = (() -> ())
+
+class ParseHelper {
+    
+    class var sharedHelper : ParseHelper {
+        struct Static {
+            static let instance:ParseHelper = ParseHelper()
+        }
+        return Static.instance
+    }
+    
+    var pfGroups:[PFObject]?
+    var groups:[Group]?
+    
+    func refresh(completion:CompletionBlock) {
+        
+        // TODO Get location
+        
+        
+        // Get all groups nearby
+        
+        // Store them somewhere so we can put them in the UI
+        
+        // Update the singers in the local group
+        
+        PFQuery(className: "Group").findObjectsInBackgroundWithBlock { (objects:[PFObject]?, error:NSError?) -> Void in
+            
+            guard let objects = objects where error == nil else {
+                print(error)
+                completion()
+                return
+            }
+            
+            let groups = CoreDataHelper.sharedHelper.groups()
+            self.groups = groups
+            self.pfGroups = objects
+            
+            for object in objects {
+                
+                var found = false
+                for group in groups {
+                    
+                    if group.name == object["name"] as? String {
+                        found = true
+                    }
+                }
+                
+                if !found {
+                    
+                    let newGroup = NSEntityDescription.insertNewObjectForEntityForName("Group", inManagedObjectContext: CoreDataHelper.managedContext) as! Group
+                    newGroup.name = object["name"] as! String
+                }
+            }
+            CoreDataHelper.sharedHelper.saveContext()
+            self.refreshSingersForSelectedGroup(completion)
+        }
+    }
+    
+    func findPFGroupMatchingGroup(group:Group) -> PFObject? {
+        
+        guard let pfGroups = self.pfGroups else { return nil }
+        for object in pfGroups {
+            if group.name == object["name"] as? String {
+                return object
+            }
+        }
+        
+        return nil
+    }
+    
+    func didChangeGroup(completion:CompletionBlock) {
+        
+        // delete all local singers
+        let fetchRequest = NSFetchRequest(entityName: "Singer")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        do {
+            try CoreDataHelper.sharedHelper.persistentStoreCoordinator!.executeRequest(deleteRequest, withContext: CoreDataHelper.managedContext)
+        } catch let error as NSError {
+            
+            let alert = UIAlertController(title: "Internal Error", message: "Error switching groups. If you see strange things, please delete the app and reinstall.\n" + error.localizedDescription, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action:UIAlertAction) -> Void in
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+        }
+
+        // reload from server
+        refreshSingersForSelectedGroup(completion)
+    }
+
+    func refreshSingersForSelectedGroup(completion:CompletionBlock) {
+        
+        guard let user = PFUser.currentUser(),
+            let group = user["group"] else {
+                completion()
+                return
+        }
+        
+        let query = group.relationForKey("singers").query()
+        query.findObjectsInBackgroundWithBlock({ (objects:[PFObject]?, error:NSError?) -> Void in
+            
+            guard let objects = objects where error == nil else {
+                print(error)
+                completion()
+                return
+            }
+            
+            let singers = CoreDataHelper.sharedHelper.singers()
+            
+            for object in objects {
+                
+                var found = false
+                for singer in singers {
+                    
+                    if singer.firstName == object["firstName"] as? String && singer.lastName == object["lastName"] as? String {
+                        found = true
+                    }
+                }
+                
+                if !found {
+                    
+                    let newSinger = NSEntityDescription.insertNewObjectForEntityForName("Singer", inManagedObjectContext: CoreDataHelper.managedContext) as! Singer
+                    newSinger.firstName = object["firstName"] as? String
+                    newSinger.lastName = object["lastName"] as? String
+                }
+            }
+            CoreDataHelper.sharedHelper.saveContext()
+        })
+    }
+    
+}
