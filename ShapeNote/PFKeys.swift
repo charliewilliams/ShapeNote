7e5893d9e339052664f5a4b2bc7ee6fb33b698ef
Charlie Williams <c@charliewilliams.org>
2016-02-08 22:55:42 +0100

    Start using an enum instead of bare strings for Parse keys

---
+++ ShapeNote/PFKeys.swift
@@ -0,0 +1,13 @@
+//
+//  PFKeys.swift
+//  ShapeNote
+//
+//  Created by Charlie Williams on 07/02/2016.
+//  Copyright © 2016 Charlie Williams. All rights reserved.
+//
+
+import Foundation
+
+enum PFKeys: String {
+    case name = "name"
+}
