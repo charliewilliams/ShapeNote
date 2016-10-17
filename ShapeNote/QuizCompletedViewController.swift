12f917be19d35b91c65a0499193dd9c878504433
Charlie Williams <c@charliewilliams.org>
2016-10-17 11:06:56 +0100

    Background image on quiz done VC

--- ShapeNote/QuizCompletedViewController.swift
+++ ShapeNote/QuizCompletedViewController.swift
@@ -16,34 +16,35 @@ class QuizCompletedViewController: UIViewController {
     @IBOutlet var headlineLabel: UILabel!
     @IBOutlet var detailLabel: UILabel!
     @IBOutlet var okButton: UIButton!
+    @IBOutlet var backgroundImageView: UIImageView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         headlineLabel.text = phraseForScore().string
-//        backgroundImageView.image = phraseForScore().image
+        backgroundImageView.image = phraseForScore().image
         detailLabel.text = "You got\n\(numberCorrect)\nout of\n\(numberOfQuestions)\nright."
     }
     
-    func phraseForScore() -> (string:String, image:String?) {
+    func phraseForScore() -> (string: String, image: UIImage?) {
         
         let percent = Float(numberCorrect) / Float(numberOfQuestions)
         
         switch percent {
         case 0.0..<0.1:
-            return ("Try again", nil)
+            return ("Try again", #imageLiteral(resourceName: "pageOpen"))
         case 0.1..<0.3:
-            return ("Tough Times", nil)
+            return ("Tough Times", #imageLiteral(resourceName: "Hosiah Parris"))
         case 0.3..<0.5:
-            return ("Nice try", nil)
+            return ("Nice try", #imageLiteral(resourceName: "Parrises"))
         case 0.5..<0.7:
-            return ("Nice work", nil)
+            return ("Nice work", #imageLiteral(resourceName: "George H Parris and Ollie Denson"))
         case 0.7..<0.9:
-            return ("Great job", nil)
+            return ("Great job", #imageLiteral(resourceName: "leading"))
         case 0.9..<1.0:
-            return ("Super!", nil)
+            return ("Super!", #imageLiteral(resourceName: "angel"))
         case 1.0:
-            return ("Perfect Score!", nil)
+            return ("Perfect Score!", #imageLiteral(resourceName: "leading"))
         default:
             fatalError()
         }
