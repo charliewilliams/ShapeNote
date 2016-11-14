98915aa0de31c85db73f43ceb6bc1e9c0df10972
Charlie Williams <c@charliewilliams.org>
2016-11-14 11:48:22 +0000

    Fix twitter login flow

--- ShapeNote/TwitterShareHelper.swift
+++ ShapeNote/TwitterShareHelper.swift
@@ -24,53 +24,57 @@ class TwitterShareHelper: NSObject {
     func postLesson(_ lesson:Lesson) {
         
         // WARNING: DEBUG
-        #if DEBUG
-        print("Not posting test run to Twitter: \(lesson.twitterString())")
-        return;
-        #endif
+//        #if DEBUG
+//        print("Not posting test run to Twitter: \(lesson.twitterString())")
+//        return
+//        #endif
 
         self.lesson = lesson
         let statusPostEndpoint = "https://api.twitter.com/1.1/statuses/update.json"
         let params = ["status": lesson.twitterString()]
         
         guard let userId = Twitter.sharedInstance().sessionStore.session()?.userID else {
+            
+            Twitter.sharedInstance().logIn { (session:TWTRSession?, error:Error?) in
+                if error == nil {
+                    self.postLesson(lesson)
+                }
+            }
+            print("No twitter user Id available")
             return
         }
+        
+        print("Tweeting as \(userId)")
         
         let client = TWTRAPIClient(userID: userId)
         var clientError : NSError? = nil
-        let request: URLRequest! = client.urlRequest(withMethod: "POST", url: statusPostEndpoint, parameters: params, error: &clientError)
+        let request = client.urlRequest(withMethod: "POST", url: statusPostEndpoint, parameters: params, error: &clientError)
         if let error = clientError {
-            print("Error connecting to Twitter: " + error.localizedDescription)
-        }
-        
-        guard request != nil else {
-            print("Error: \(clientError)")
-            return
+            print("Error connecting to Twitter for user \(userId): \(error.localizedDescription)")
         }
         
         client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
             
-            if let data = data, connectionError == nil {
-                var jsonError : NSError?
-                let json : Any?
-                do {
-                    json = try JSONSerialization.jsonObject(with: data, options: [])
-                } catch let error as NSError {
-                    jsonError = error
-                    json = nil
-                } catch {
-                    fatalError()
-                }
-                
-                if jsonError != nil {
-                    print("Error: \(jsonError)")
-                } else {
-                    print("\(json)")
-                }
-            }
-            else {
+            guard let data = data, connectionError == nil else {
                 print("Error: \(connectionError)")
+                return
+            }
+            
+            var jsonError : NSError?
+            let json : Any?
+            do {
+                json = try JSONSerialization.jsonObject(with: data, options: [])
+            } catch let error as NSError {
+                jsonError = error
+                json = nil
+            } catch {
+                fatalError()
+            }
+            
+            if jsonError != nil {
+                print("Error: \(jsonError)")
+            } else {
+                print("\(json)")
             }
         }
     }
